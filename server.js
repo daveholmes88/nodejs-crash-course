0576a64ea342833dc9a8002b3dc81dedcc776827
daveholmes88 <github email address>
2021-01-21 14:04:35 -0600

    finish lesson 6

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((requestObj, responseObj) => {
+
+    // lodash
+    // const num = _.random(0, 20)
+
+    // console.log(num)
+
+    // const greet = _.once(() => {
+    //     console.log('hello')
+    // })
+
+    // greet()
+    // greet()
     // console.log(requestObj.url, requestObj.method)
 
     // set header content type
